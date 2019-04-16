9e7274e883c16703449066c8373b51e97adffe49
Adam Hurd <c3330727@uon.edu.au>
2019-04-16 09:07:30 +0000

    Major overhaul changed the arrangement of the code and it now seems to work, but is killed at the end still, but if it works why change it

--- Rotational Cipher encryption.c
+++ Rotational Cipher encryption.c
@@ -1,45 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-//asfds
-char encryptionEquation(char c);
+#include<stdio.h>
+ 
+int main(){
 
-int main(void)
-{
-    char rotationAmount;
-    char encryptedResult;
-    int numberOfCharacters = 0;
-    char message[1000];
-    int counter = 0;
-    char n = 0;
-    int c;
-    
-    
-    printf(" Select rotation amount: ");
-    scanf("%d", &rotationAmount);
-    printf(" Write a message in uppercase characters: ");
-    fgets(message, 1000, stdin);
-    scanf ("%[^\n]%*c", message);
-    
-    numberOfCharacters = strlen(message);
-    
-        printf(" Amount of characters in message: %d\n", numberOfCharacters);
+	char message[300];
+	char c;
+	int letter;
+	int RotationAmount;
+	int n;
+	
+	printf("Enter a message to encrypt: ");
+	gets(message);
+	printf("Enter rotation amount: ");
+	scanf("%d", &RotationAmount);
+	
+	for(letter = 0; message[letter] != '\0'; ++letter){
+		
+		
+		c = message[letter];
+		
+		if(c >= 65 && c <= 90){
+			
+			n = c + RotationAmount;
+			
+		if(n > 90){
+			n = n - 26;
+		}
+			
+		message[letter] = n;
+		}
+	}
+	
+printf("Encrypted message: %s/n", message);
 
-    printf(" Encrypted message: "); 
-     
-     for(n = 0; n < numberOfCharacters ; n++){
-    
-        c = message[n] + rotationAmount ;
-        
-         if(c > 32 && c < 61){
-            c = c - rotationAmount;
-        }
-        if(c > 90){
-            c = c - 26;
-        }
-        
-        
-        printf("%c", c);
-     }
-    
-   
 }
