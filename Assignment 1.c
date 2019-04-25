3c216aadc99faef58ccf4129df255788950a886d
Adam Hurd <c3330727@uon.edu.au>
2019-04-25 02:36:10 +0000

    added comments

--- Assignment 1.c
+++ Assignment 1.c
@@ -9,7 +9,6 @@ int main(){
 	int n; //temporary variable used to 
 	int selection = 0; //user interface selection variable
 	
-	
 	printf("For Rotational Cipher encryption of message press 1\n");
 	printf("For Rotational Cipher decryprion of message press 2\n");
 	printf("For Substitution Cipher encryption press 3\n");
@@ -30,18 +29,15 @@ int main(){
 	
     for(letter = 0; message[letter] != '\0'; ++letter){ //for loop allows each letter to be rotated, the != '\0' tells the for loop to stop at the end of the string
 		
-		
 		c = message[letter]; //message[letter] is assigned to c as a temp variable to allow for rotation, as it converts the letters into their ASCII equivilent
 		
 		if(c >= 65 && c <= 90){ //This only allows roation of ASCII character of values 65-90 or A-Z
 			
 			n = c + RotationAmount; //the rotation happens then it is assigned to a second temp variable "n"
 		
-
 		if(n > 90){ //this if statement stops a character being rotated past the letters on the ASCII table eg 90 which is Z
 			n = n - 26; // This bring the character back to the alphebet
 		}
-		
 		
         message[letter] = n; //reassigns n to message[letter] so it can be printed as a string
         }
@@ -50,9 +46,6 @@ int main(){
 	printf("Encrypted message: %s\n", message); //prints encrypted message
 
 }
-
-	
-
 
 if(selection == 2){
     
@@ -66,25 +59,20 @@ if(selection == 2){
 	    return 0;
 }
 
-	
 	for(letter = 0; message[letter] != '\0'; ++letter){
-		
 		
 		c = message[letter];
 		
 		if(c >= 65 && c <= 90){
 			
-			n = c - RotationAmount;
+			n = c - RotationAmount; //the rotation amount is negative to rotate back to orginal
 			
 		if(n < 65){
-			n = n + 26;
-		}
-		
-		if(message[n] == 32 && message[n+1] == 32){
-            break;
-        }
-        
-		message[letter] = n;
+			n = n + 26; //positive 26 as the cipher is rotated negativly
+	    }
+	    
+	    message[letter] = n;
+	    
 		}
 	}
 	
@@ -95,22 +83,15 @@ printf("Decrypted message: %s\n", message);
 
     printf("Enter a message to encrypt: ");
 	gets(message);
-	
-	
-	
-	printf("Decrypted message: ");
-
+	printf("Decrypted message: "); //the print statement needs to be before the substition process
     
 	for(letter = 0; message[letter] != '\0'; ++letter){
 		
-		
-		//c = message[letter];
-		
-		switch(message[letter]){
+		switch(message[letter]){ //switch statement allows each letter to substituted
     
     case 'A':
-        printf("S");
-        break;
+        printf("S"); //encrypted letter is printed each time the original letter is put through the for loop
+        break; //break statement is used to stop each case from being executed
     case 'B':
         printf("Q");
         break;
@@ -126,129 +107,89 @@ printf("Decrypted message: %s\n", message);
     case 'F':
         printf("X") ;
         break;
-        
     case 'G':
        printf("Y") ;
         break;
-        
     case 'H':
         printf("N") ;
         break;
-    
     case 'I':
         printf("C") ;
         break;
-        
     case 'J':
         printf("H") ;
         break;
-        
     case 'K':
         printf("I") ;
         break;
-        
     case 'L':
         printf("U") ;
         break;
-        
     case 'M':
        printf("V") ;
         break;
-    
     case 'N':
         printf("Z") ;
         break;
-    
     case 'O':
         printf("J") ;
         break;
-    
     case 'P':
         printf("O") ;
         break;
-    
     case 'Q':
         printf("B") ;
         break;
-        
     case 'R':
         printf("T") ;
         break;
-    
     case 'S':
         printf("M") ;
         break;
-    
     case 'T':
         printf("G") ;
         break;
-    
     case 'U':
         printf("R") ;
         break;
-    
     case 'V':
         printf("W") ;
         break;
-    
     case 'W':
         printf("A") ;
         break;
-    
     case 'X':
         printf("F") ;
         break;
-        
     case 'Y':
         printf("E") ;
         break;
-    
     case 'Z':
         printf("D") ;
         break;
-        
     case ' ':
         printf(" ");
         break;
     case '!':
         printf("!");
         break;
-   
-		
 	}
-
-		
-		if(message[letter] == 32 && message[letter+1] == 32){
-        break;
-        }
-        
-		
-		
 	}
-	
-    printf("\n");
-
- 
+	printf("\n"); //a new line is printed to optimise the output
 }
 
 if(selection == 4){
     
     printf("Enter a message to decrypt: ");
 	gets(message);
-	
-	
-	
 	printf("Decrypted message: ");
 
 for(letter = 0; message[letter] != '\0'; ++letter){
 		
-		
-		
-		
-		switch(message[letter]){
+		switch(message[letter]){ //switch statement decrypts the substitution cipher
     
     case 'S':
-        printf("A");
+        printf("A"); //printing the decrypted letters
         break;
     case 'Q':
         printf("B");
@@ -265,108 +206,74 @@ for(letter = 0; message[letter] != '\0'; ++letter){
     case 'X':
         printf("F") ;
         break;
-        
     case 'Y':
        printf("G") ;
         break;
-        
     case 'N':
         printf("H") ;
         break;
-    
     case 'C':
         printf("I") ;
         break;
-        
     case 'H':
         printf("J") ;
         break;
-        
     case 'I':
         printf("K") ;
         break;
-        
     case 'U':
         printf("I") ;
         break;
-        
     case 'V':
        printf("M") ;
         break;
-    
     case 'Z':
         printf("N") ;
         break;
-    
     case 'J':
         printf("O") ;
         break;
-    
     case 'O':
         printf("P") ;
         break;
-    
     case 'B':
         printf("Q") ;
         break;
-        
     case 'T':
         printf("R") ;
         break;
-    
     case 'M':
         printf("S") ;
         break;
-    
     case 'G':
         printf("T") ;
         break;
-    
     case 'R':
         printf("U") ;
         break;
-    
     case 'W':
         printf("V") ;
         break;
-    
     case 'A':
         printf("W") ;
         break;
-    
     case 'F':
         printf("X") ;
         break;
-        
     case 'E':
         printf("Y") ;
         break;
-    
     case 'D':
         printf("Z") ;
         break;
-        
     case ' ':
         printf(" ");
         break;
     case '!':
         printf("!");
         break;
-   
-		
-	}
-
-		
-		if(message[letter] == 32 && message[letter+1] == 32){
-        break;
-        }
-        
-		
-		
+	}	
 	}
     printf("\n");
-
-
-
 }
 }
